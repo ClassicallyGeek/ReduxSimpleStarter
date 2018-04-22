b26922080afd58769acb9df287dbc77e69384314
Ashley <AshleyReneeLowry@gmail.com>
2018-04-22 08:37:31 -0500

    callback to select video from list to display in app

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 // the first arguement in the props array has a video property- please grab that and call it video.
-const VideoListItem = ( {video} ) => {
+const VideoListItem = ( {video, onVideoSelect} ) => {
   const imageUrl = video.snippet.thumbnails.default.url;
   const title = video.snippet.title;
   return (
-    <li className="list-group-item">
+    <li className="list-group-item" onClick={() => onVideoSelect(video)}>
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object" src={imageUrl}/>
--- src/index.js
+++ src/index.js
@@ -16,21 +16,31 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({
       key: API_KEY,
       term: 'kittens'
-    }, videos => this.setState({ videos }) );
-
+    }, videos => this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
+   );
   }
+
+
    // html stuff is JSX
   render() {
     return ( // Multi-line JSX with ()
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect= { selectedVideo => this.setState({selectedVideo})}/>
       </div>
     );
   }
